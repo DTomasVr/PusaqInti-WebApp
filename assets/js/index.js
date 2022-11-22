0bbae5ff9c6388f2433c2bd53dc60d2f188c0f76
DTomasVr <diego.villafuerte.2033@gmail.com>
2022-11-22 16:24:55 -0500

    Add links and bug fixes
    -Add links
    -Fixing bugs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,20 @@
-const navToggle = document.querySelector(".nav-toggle")
-const navMenu = document.querySelector(".nav-menu")
+const navToggle = document.querySelector(".nav-toggle1")
+const navMenu = document.querySelector(".nav-menu1")
 
 navToggle.addEventListener("click",()=>{
-    navMenu.classList.toggle("nav-menu_visible");
+    navMenu.classList.toggle("nav-menu_visible1");
     
-    if (navMenu.classList.contains("nav-menu_visible")) {
+    if (navMenu.classList.contains("nav-menu_visible1")) {
         navToggle.setAttribute("aria-label", "Cerrar menú");
       } else {
         navToggle.setAttribute("aria-label", "Abrir menú");
       }
 });
 
-const menuLinks = document.querySelectorAll('.nav-menu a[href^="#"');
+const menuLinks = document.querySelectorAll('.nav-menu1 a[href^="#"');
 
 menuLinks.forEach(menuLinks => {
   menuLinks.addEventListener("click",function(){
-    navMenu.classList.toggle("nav-menu_visible")
+    navMenu.classList.toggle("nav-menu_visible1")
   })
 })
